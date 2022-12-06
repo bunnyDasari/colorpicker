3d483ef629169022f72401593fbc61e4cd56a3f5
Dasari Rohan <104191071+bunnyDasari@users.noreply.github.com>
2022-12-06 23:26:03 +0530

    Add files via upload

---
+++ colorpicker.js
@@ -0,0 +1,27 @@
+let background_color = document.getElementById("colorPickerContainer");
+let selectColorHexcodeElemnt = document.getElementById("selectedColorHexCode");
+
+function buttonOne() {
+    background_color.style.backgroundColor = "#e0e0e0";
+    selectColorHexcodeElemnt.textContent = "#e0e0e0";
+
+
+
+}
+
+function buttonTwo() {
+    background_color.style.backgroundColor = "#6fcf97";
+    selectColorHexcodeElemnt.textContent = "#6fcf97";
+
+}
+
+function buttonThree() {
+    background_color.style.backgroundColor = "#56ccf2";
+    selectColorHexcodeElemnt.textContent = "#56ccf2";
+
+}
+
+function buttonFour() {
+    background_color.style.backgroundColor = "#bb6bd9";
+    selectColorHexcodeElemnt.textContent = "#bb6bd9";
+}
